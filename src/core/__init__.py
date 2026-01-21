4e18a83315a6938e552f7a7874bb9eb257884e48
sa1vador77 <uncrowned.blessed@gmail.com>
2026-01-21 06:36:25 +0300

    Написан файл логирования

---
+++ src/core/__init__.py
@@ -0,0 +1,8 @@
+from .config import config
+from .logger import setup_logger
+
+
+__all__ = [
+    "config",
+    "setup_logger",
+]
---
+++ src/core/logger.py
@@ -0,0 +1,20 @@
+import sys
+from loguru import logger
+
+
+def setup_logger():
+    """Настройка конфигурации логгера Loguru."""
+    logger.remove()
+    
+    # Формат логов: время | уровень | модуль:строка | сообщение
+    log_format = (
+        "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | "
+        "<level>{level: <8}</level> | "
+        "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - "
+        "<level>{message}</level>"
+    )
+    
+    logger.add(sys.stderr, format=log_format, level="INFO")
+    logger.add("logs/bot.log", rotation="10 MB", compression="zip", level="DEBUG")
+    
+    logger.info("Логгер успешно инициализирован")
